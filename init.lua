67fa7f32102e49985e7f8278ade1b8e8280a227c
Tabb <66449270+tTabbZIn@users.noreply.github.com>
2024-11-07 21:29:51 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -25,6 +25,66 @@ require('packer').startup(function(use)
   }
 
 
+vim.opt.number = true
+
+
+-- Configuracoes gerais
+--
+
+-- Habilitar realce de sintaxe
+vim.cmd('syntax on')
+
+-- Habilitar suporte ao clipboard
+vim.opt.clipboard:append('unnamedplus')
+
+-- Habilitar números de linha
+vim.opt.number = true
+
+vim.opt.tabstop = 4          -- Tamanho de tabulação visual de 4 espaços
+vim.opt.softtabstop = 4      -- Tamanho de tabulação em espaços ao editar
+vim.opt.shiftwidth = 4       -- Tamanho de indentação ao usar '>'
+vim.opt.expandtab = true     -- Converter tab em espaços
+vim.opt.smarttab = true      -- Inserir tabs no início da linha de acordo com shiftwidth
+
+-- Configurações de indentação
+vim.opt.smartindent = true   -- Inserir automaticamente um nível extra de indentação em alguns casos
+
+-- Configurações de buffer
+vim.opt.hidden = true        -- Permitir ocultar o buffer atual ao abrir um novo arquivo
+
+-- Configurações de busca
+vim.opt.incsearch = true     -- Busca incremental
+vim.opt.ignorecase = true    -- Ignorar maiúsculas/minúsculas na busca
+vim.opt.smartcase = true     -- Considerar maiúsculas se houver uma letra maiúscula na busca
+
+-- Configurações de rolagem e interface
+vim.opt.scrolloff = 8        -- Número mínimo de linhas acima e abaixo do cursor
+vim.opt.cmdheight = 2        -- Mais espaço para exibir mensagens
+vim.opt.updatetime = 100     -- Tempo em milissegundos para considerar mudanças
+
+-- Configurações de codificação e backup
+vim.opt.encoding = 'utf-8'   -- Codificação em UTF-8 para suportar ícones de fonte
+vim.opt.backup = false       -- Não criar arquivos de backup
+vim.opt.writebackup = false  -- Não criar backup antes de salvar
+
+-- Comportamento de divisões (splits)
+vim.opt.splitright = true    -- Divisões verticais à direita
+vim.opt.splitbelow = true    -- Divisões horizontais abaixo
+
+-- Atualizar automaticamente o arquivo ao detectar mudanças externas
+vim.opt.autoread = true
+
+-- Habilitar suporte ao mouse
+vim.opt.mouse = 'a'
+
+-- Configurações de tipo de arquivo
+vim.cmd('filetype on')
+vim.cmd('filetype plugin on')
+vim.cmd('filetype indent on')
+
+
+
+
 
   -- Configuração do ToggleTerm
 require("toggleterm").setup()
